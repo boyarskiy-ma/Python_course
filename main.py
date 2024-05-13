5230aae58bc823fe0c31190eba513a4a788e2e9f
boyarskiy.ma <pochta@boyarskogo.ru>
2024-05-13 17:11:41 +0300

    add_new_inforna_information

--- main.py
+++ main.py
@@ -121,10 +121,24 @@
 #         line += "*"
 #     print(line)
 
-text = 'СъЕШЬ ещё этих МяГкИх французских булок'
-print(len(text)) # 39
+# text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+# print(len(text)) # 39
 # print('ещё' in text) # True
 # print(text.lower()) # съешь ещё этих мягких французских булок
 # print(text.upper()) # СЪЕШЬ ЕЩЁ ЭТИХ МЯГКИХ ФРАНЦУЗСКИХ БУЛОК
 # print(text.replace('ещё','ЕЩЁ')) # СъЕШЬ ЕЩЁ этих МяГкИх французских булок
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-5]) # б
+# print(text[:]) # съешь ещё этих мягких французских булок
+# print(text[:2]) # съ
+# print(text[len(text)-2:]) # ок
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ...
 
