9e3dd1f0c1f1e7c2bf58c4e249ce0681addabe08
boyarskiy.ma <pochta@boyarskogo.ru>
2024-05-29 13:46:41 +0300

    add_new_inforna_information

--- main.py
+++ main.py
@@ -177,8 +177,23 @@
 #         e += 1
 #     print(c)
 # else:
-#     print(-1)
+#     print("-1")
 
+# a = int(input('Введите число: '))
+# b = 1
+# c = 0
+# d = 0
+# e = 0
+# while d < a:
+#     d = b + c
+#     b = c
+#     c = d
+#     e += 1
+#     if d > a:
+#         print("-1")
+#     elif d == a:
+#         print(e)
+    
 # list_1 = []
 # list_2 = ()
 
@@ -238,4 +253,46 @@
 # for e in t:
 #     print(e) # red green blue
 # t[0] = 'black' # TypeError: 'tuple' object does not support(нельзя изменятькортеж)
+
+# dictionary = {}
+# dictionary ={'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+# print(dictionary['left']) # ←
+# # типы ключей могут отличаться
+# print(dictionary['up']) # ↑
+# # типы ключей могут отличаться
+# dictionary['left'] = '⇐'
+# print(dictionary['left']) # ⇐
+# print(dictionary['type']) # KeyError: 'type'
+# del dictionary['left'] # удаление элемента
+# for item in dictionary: # for (k,v) in dictionary.items():
+# print('{}: {}'.format(item, dictionary[item]))
+# # up: ↑
+# # down: ↓
+# # right: →
+
+# colors = {'red', 'green', 'blue'}
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('red')
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('gray')
+# print(colors) # {'red', 'green', 'blue','gray'}
+# colors.remove('red')
+# print(colors) # {'green', 'blue','gray'}
+# colors.remove('red') # KeyError: 'red'
+# colors.discard('red') # ok
+# print(colors) # {'green', 'blue','gray'}
+# colors.clear() # { }
+# print(colors) # set()
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy() # c = {1, 2, 3, 5, 8}
+# u = a.union(b) # u = {1, 2, 3, 5, 8, 13,
+# i = a.intersection(b) # i = {8, 2, 5}
+# dl = a.difference(b) # dl = {1, 3}
+# dr = b.difference(a) # dr = {13, 21}
+# q=a.union(b).difference(a.intersection(b)) # {1, 21, 3, 13}
+
+# list_1 = [i for i in range(1, 101)] # [1, 2, 3,..., 100]
 
