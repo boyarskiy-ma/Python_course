baf987cbc69174e15971efeacb71a95e77b23507
boyarskiy.ma <pochta@boyarskogo.ru>
2024-05-29 12:31:42 +0300

    add_new_inforna_information

--- main.py
+++ main.py
@@ -192,7 +192,7 @@
 
 # list_1 = [1, 2, 3]
 # print(list_1)
-# list_1.append(4)
+#     list_1.append(4)
 # print(list_1)
 
 # list_1 = []
@@ -217,7 +217,6 @@
 # print(list_1[:2]) # [1, 2]
 # print(list_1[len(list_1)-2:]) #[9, 10]
 # print(list_1[2:9]) # [3, 4, 5, 6, 7, 8, 9]
-# print(list_1[6:-18]) # []
 # print(list_1[0:len(list_1):6]) # [1, 7]
 # print(list_1[::6]) # [1, 7]
 
